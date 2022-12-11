20d256ec74901ab33c04635e77e6c1018006f074
Kuloreza <rezafebri158@gmail.com>
2022-12-11 14:46:33 +0700

    zaaaaa

---
+++ plugins/info-script.js
@@ -0,0 +1,24 @@
+const fetch = require('node-fetch')
+let handler = async(m, { conn, text, usedPrefix, command }) => {
+let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
+
+let str = `*wa.me/62895412779117* 
+
+let wibu = `https://api-reysekha.herokuapp.com/api/random/cosplay?apikey=apirey` 
+let thumb = await(await fetch(wibu)).buffer()
+conn.sendButtonDoc(m.chat, str, wm,'O W N E R','.owner', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
+    mediaUrl: "-",
+    mediaType: "VIDEO",
+    description: "-", 
+    title: 'OptimumPred',
+    body: wm,
+    thumbnail: thumb,
+    sourceUrl: urlnya
+  }
+  } }) 
+          }
+handler.help = ['script']
+handler.tags = ['info']
+handler.command =  /^(script|sc)$/i
+
+module.exports = handler
---
+++ plugins/main-menu2.js
@@ -0,0 +1,31 @@
+const fs = require('fs')
+const fetch = require('node-fetch')
+let handler = async(m, { conn, text, usedPrefix, command }) => {
+let tag = `wa.me/62895412779117`
+  let mentionedJid = [m.sender]
+let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
+let suka = `SayaRezaaaa`
+global.fkontak = { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: `status@broadcast` } : {}) }, message: { 'contactMessage': { 'displayName': suka, 'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:XL;${suka},;;;\nFN:${suka},\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabell:Ponsel\nEND:VCARD`, 'jpegThumbnail': fs.readFileSync('./thumbnail.jpg'), thumbnail: fs.readFileSync('./thumbnail.jpg'),sendEphemeral: true}}}
+
+let str = `*Hai Ngab*`
+let isi =`I'm OptimumPred.\nKalau Ada Yg Error Bisa Langsung Report Ke Owner.\n${tag} (Owner)`
+let wibu = `https://api-reysekha.herokuapp.com/api/random/cosplay?apikey=apirey` 
+let thumb = await(await fetch(wibu)).buffer()
+conn.send3ButtonImg(m.chat, `https://telegra.ph/file/3cab55bb49d4fdfbd4291.jpg`, str, isi, 'Menu', '.command', 'Owner', '.owner', 'Credit', '.credit', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
+    mediaUrl: '-',
+    mediaType: 2, 
+    description: urlnya,
+    title: "Subs Yete Gw Banh:)",
+    body: wm,
+    thumbnail: thumb,
+    sourceUrl: '-'
+     }}
+  })
+          }
+handler.help = ['menu']
+handler.tags = ['main']
+handler.command =  /^(menu)$/i
+
+handler.register = false
+
+module.exports = handler
